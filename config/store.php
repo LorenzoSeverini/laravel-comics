7b4fe96a91392692b47506682cac1db1c614124b
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-27 17:18:56 +0200

    fix store with links for header and footer

--- config/store.php
+++ config/store.php
@@ -1,19 +1,151 @@
 <?php
 
 return [
-    'someLinks' => [
+    'links' => [
         [
-            "href" => "/",
-            "text" => "Home Page",
+            "href" => "/characthers",
+            "text" => "characthers",
         ],
         [
-            "href" => "/otherpage",
-            "text" => "Other Page",
+            "href" => "/comics",
+            "text" => "Comics",
         ],
         [
-            "href" => "https://laravel.com/docs/9.x",
-            "text" => "Documentation",
-        ]
+            "href" => "/movies",
+            "text" => "Movies",
+        ],
+        [
+            "href" => "/tv",
+            "text" => "TV",
+        ],
+        [
+            "href" => "/games",
+            "text" => "Games",
+        ],
+        [
+            "href" => "/collectibles",
+            "text" => "Collectibles",
+        ],
+        [
+            "href" => "/videos",
+            "text" => "Videos",
+        ],
+        [
+            "href" => "/fans",
+            "text" => "Fans",
+        ],
+        [
+            "href" => "/news",
+            "text" => "News",
+        ],
+        [
+            "href" => "/shop",
+            "text" => "Shop",
+        ],
     ],
-    'someOtherData' => [ 42, 37, 73],
+
+    'footerNavLinks' => [
+        [
+            'name' => 'DC Comics',
+            'link' => '#',
+            'subMenu' => [
+                [
+                    'name' => 'characthers',
+                    'href' => '/characthers'
+                ],
+                [
+                    'name' => 'Comics',
+                    'href' => '/comics'
+                ],
+                [
+                    'name' => 'Movies',
+                    'href' => '/movies'
+                ],
+                [
+                    'name' => 'TV',
+                    'href' => '/tv'
+                ],
+                [
+                    'name' => 'Games',
+                    'href' => '/games'
+                ],
+                [
+                    'name' => 'Videos',
+                    'href' => '/videos'
+                ],
+                [
+                    'name' => 'News',
+                    'href' => '/news'
+                ],
+            ],
+        ],
+
+        [
+            'name' => 'Shop',
+            'link' => '#',
+            'subMenu' => [
+
+                [
+                    'name' => 'Shop DC',
+                    'href' => '/shop'
+                ],
+                [
+                    'name' => 'Shop DC Collectibles',
+                    'href' => '/collectibles'
+                ],
+            ],
+        ],
+
+        [
+            'name' => 'DC',
+            'link' => '#',
+            'subMenu' => [
+
+                [
+                    'name' => 'Terms Of Use',
+                    'href' => '#'
+                ],
+                [
+                    'name' => 'Privacy policy (New)',
+                    'href' => '#'
+                ],
+                [
+                    'name' => 'Ad Choices',
+                    'href' => '#'
+                ],
+                [
+                    'name' => 'Advertising',
+                    'href' => '#'
+                ],
+                [
+                    'name' => 'Jobs',
+                    'href' => '#'
+                ],
+                [
+                    'name' => 'Subscriptions',
+                    'href' => '#'
+                ],
+                [
+                    'name' => 'Talent Workshops',
+                    'href' => '#'
+                ],
+                [
+                    'name' => 'CPSC Certificates',
+                    'href' => '#'
+                ],
+                [
+                    'name' => 'Ratings',
+                    'href' => '#'
+                ],
+                [
+                    'name' => 'Shop Help',
+                    'href' => '#'
+                ],
+                [
+                    'name' => 'Contact Us',
+                    'href' => '#'
+                ],
+            ],
+        ],
+    ],
 ];
