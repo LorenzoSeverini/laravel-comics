8a2a345c3060b38384733da58ef192017015ccbd
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-27 17:20:48 +0200

    add partial header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,31 @@
-<header class="p-3">
-    <img class="logo" src="{{ Vite::asset('resources/img/logo.png') }}" alt="Laravel Logo">
-    <span>Some links: </span>
-    @foreach ($links as $link)
-        <a href="{{ $link['href'] }}">{{ $link['text'] }}</a>
-    @endforeach
+<header>
+    <div class="navbar navbar-expand navbar-white bg-white p-3">
+        <a class="navbar-brand" href="#">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="logo" width="50" height="50">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                @foreach($links as $link)
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ $link['href'] }}">{{ $link['text'] }}</a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
 </header>
+
+<style>
+    header {
+        background-color: #fff;
+    }
+
+    .navbar {
+        max-width: 75rem;
+        margin: 0 auto;
+    }
+</style>
+
