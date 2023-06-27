7452d8fb0da6fd6538fc27d3644c743d4c8f3e49
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-27 17:19:22 +0200

    add page for characthers link

---
+++ resources/views/characthers.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('page-title', 'Title Override: Characthers Page')
+@section('content')
+<div class="container my-3">
+    <h1>Characthers Page</h1>
+    <div class="row g-4">
+        <div class="col">
+            <div>
+                <p>This is the characthers page, with a different content for sure</p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
